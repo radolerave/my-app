8823fcefd9449526d0007a6333f8f6fd7a0a08e5
radolerave <radolerave@gmail.com>
2023-08-07 10:28:55 +0300

    jsoneditor and dexie test

--- src/js/capacitor-welcome.js
+++ src/js/capacitor-welcome.js
@@ -1,142 +1,117 @@
 import { SplashScreen } from '@capacitor/splash-screen';
 import { Camera } from '@capacitor/camera';
+import { JSONEditor } from '@json-editor/json-editor';
+import { Dexie } from 'dexie'
 
-window.customElements.define(
-  'capacitor-welcome',
-  class extends HTMLElement {
-    constructor() {
-      super();
+const element = document.getElementById('editor_holder');
 
-      SplashScreen.hide();
-
-      const root = this.attachShadow({ mode: 'open' });
-
-      root.innerHTML = `
-    <style>
-      :host {
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-        display: block;
-        width: 100%;
-        height: 100%;
-      }
-      h1, h2, h3, h4, h5 {
-        text-transform: uppercase;
-      }
-      .button {
-        display: inline-block;
-        padding: 10px;
-        background-color: #73B5F6;
-        color: #fff;
-        font-size: 0.9em;
-        border: 0;
-        border-radius: 3px;
-        text-decoration: none;
-        cursor: pointer;
-      }
-      main {
-        padding: 15px;
-      }
-      main hr { height: 1px; background-color: #eee; border: 0; }
-      main h1 {
-        font-size: 1.4em;
-        text-transform: uppercase;
-        letter-spacing: 1px;
-      }
-      main h2 {
-        font-size: 1.1em;
-      }
-      main h3 {
-        font-size: 0.9em;
-      }
-      main p {
-        color: #333;
-      }
-      main pre {
-        white-space: pre-line;
-      }
-    </style>
-    <div>
-      <capacitor-welcome-titlebar>
-        <h1>Capacitor</h1>
-      </capacitor-welcome-titlebar>
-      <main>
-        <p>
-          Capacitor makes it easy to build powerful apps for the app stores, mobile web (Progressive Web Apps), and desktop, all
-          with a single code base.
-        </p>
-        <h2>Getting Started</h2>
-        <p>
-          You'll probably need a UI framework to build a full-featured app. Might we recommend
-          <a target="_blank" href="http://ionicframework.com/">Ionic</a>?
-        </p>
-        <p>
-          Visit <a href="https://capacitorjs.com">capacitorjs.com</a> for information
-          on using native features, building plugins, and more.
-        </p>
-        <a href="https://capacitorjs.com" target="_blank" class="button">Read more</a>
-        <h2>Tiny Demo</h2>
-        <p>
-          This demo shows how to call Capacitor plugins. Say cheese!
-        </p>
-        <p>
-          <button class="button" id="take-photo">Take Photo</button>
-        </p>
-        <p>
-          <img id="image" style="max-width: 100%">
-        </p>
-      </main>
-    </div>
-    `;
-    }
-
-    connectedCallback() {
-      const self = this;
-
-      self.shadowRoot.querySelector('#take-photo').addEventListener('click', async function (e) {
-        try {
-          const photo = await Camera.getPhoto({
-            resultType: 'uri',
-          });
-
-          const image = self.shadowRoot.querySelector('#image');
-          if (!image) {
-            return;
-          }
-
-          image.src = photo.webPath;
-        } catch (e) {
-          console.warn('User cancelled', e);
+const editor = new JSONEditor(element, {
+  use_name_attributes: false,
+  theme: 'bootstrap4',
+  disable_edit_json: true,
+  disable_properties: true,
+  disable_collapse: true,
+  schema: {
+    'title': 'Person',
+    'type': 'object',
+    'required': [
+      'name',
+      'age',
+      'date',
+      'favorite_color',
+      'gender',
+      'location',
+      'pets'
+    ],
+    'properties': {
+      'name': {
+        'type': 'string',
+        'description': 'First and Last name',
+        'minLength': 4,
+        'default': 'Jeremy Dorn'
+      },
+      'age': {
+        'type': 'integer',
+        'default': 25,
+        'minimum': 18,
+        'maximum': 99
+      },
+      'favorite_color': {
+        'type': 'string',
+        'format': 'color',
+        'title': 'favorite color',
+        'default': '#ffa500'
+      },
+      'gender': {
+        'type': 'string',
+        'enum': [
+          'male',
+          'female',
+          'other'
+        ]
+      },
+      'date': {
+        'type': 'string',
+        'format': 'date',
+        'options': {
+          'flatpickr': {}
         }
-      });
+      },
+      'location': {
+        'type': 'object',
+        'title': 'Location',
+        'properties': {
+          'city': {
+            'type': 'string',
+            'default': 'San Francisco'
+          },
+          'state': {
+            'type': 'string',
+            'default': 'CA'
+          },
+          'citystate': {
+            'type': 'string',
+            'description': 'This is generated automatically from the previous two fields',
+            'template': '{{city}}, {{state}}',
+            'watch': {
+              'city': 'location.city',
+              'state': 'location.state'
+            }
+          }
+        }
+      },
+      'pets': {
+        'type': 'array',
+        'format': 'table',
+        'title': 'Pets',
+        'uniqueItems': true,
+        'items': {
+          'type': 'object',
+          'title': 'Pet',
+          'properties': {
+            'type': {
+              'type': 'string',
+              'enum': [
+                'cat',
+                'dog',
+                'bird',
+                'reptile',
+                'other'
+              ],
+              'default': 'dog'
+            },
+            'name': {
+              'type': 'string'
+            }
+          }
+        },
+        'default': [
+          {
+            'type': 'dog',
+            'name': 'Walter'
+          }
+        ]
+      }
     }
   }
-);
-
-window.customElements.define(
-  'capacitor-welcome-titlebar',
-  class extends HTMLElement {
-    constructor() {
-      super();
-      const root = this.attachShadow({ mode: 'open' });
-      root.innerHTML = `
-    <style>
-      :host {
-        position: relative;
-        display: block;
-        padding: 15px 15px 15px 15px;
-        text-align: center;
-        background-color: #73B5F6;
-      }
-      ::slotted(h1) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-        font-size: 0.9em;
-        font-weight: 600;
-        color: #fff;
-      }
-    </style>
-    <slot></slot>
-    `;
-    }
-  }
-);
+});
